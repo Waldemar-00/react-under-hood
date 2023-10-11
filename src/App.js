8fe7f5ce9b68a45a9eadcca5c15a82a7b0f33861
uladzimir <01myit@gmail.com>
2023-10-11 21:32:05 +0300

    closure and useCallback

--- src/App.js
+++ src/App.js
@@ -5,9 +5,13 @@ import Article from './components/Out/Article'
 import { useState, useCallback } from 'react'
 function App() {
   const [isShow, setIsShow] = useState(false)
+  const [toggleActive, setToggleActive] = useState(false)
   const toggleParagraph = useCallback(() => {
-    setIsShow(isShow => !isShow)
-  }, [])
+    if (toggleActive) setIsShow(isShow => !isShow)
+  }, [toggleActive])
+  const toggleActivate = () => {
+    setToggleActive(toggleActivate => !toggleActivate)
+  }
   return (
     <div className="App">
       <h1>React under the hood</h1>
@@ -15,6 +19,7 @@ function App() {
       <Button foo={toggleParagraph}>
         Toggle paragraph
       </Button>
+      <Button foo={toggleActivate}>Toggle Activate</Button>
       <Article show={false}/>
     </div>
   ) 
